9b0379d18cdaaf5d4bae9024f5e6e35452bc3c72
Shama Pathan <suspathan@gmail.com>
2019-10-28 16:53:09 +0600

    Added Wishlist page

---
+++ dashboard.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<?php include('section/head.php');?>
+
+<body>
+
+   <!--Offcanvas menu area start-->
+    <?php include("section/offcanvas.php");?>
+    <!--Offcanvas menu area end-->
+    
+    <!--header area start-->
+    <div style="margin-left:4em;margin-right:4em">
+        <?php include("section/header.php");?>
+    </div>
+    <!-- Header Area ends -->
+
+    
+    <!-- Dashboard  Start-->
+    <div style="margin-left:4em;margin-right:4em">
+    <?php include("section/dashboard.php");?>
+    </div>
+<!-- Dashboard Ends -->
+    <!--footer area start-->
+    <?php include("section/footer.php");?>
+    <!--footer area end-->
+
+    <!-- modal area start-->
+    <?php include("section/modelArea.php");?>
+    <!-- modal area end-->
+
+    <!--news letter popup start-->
+     <?php include("section/newsLetter.php");?>
+    <!--news letter popup start-->
+    <?php include("section/script.php");?>
+    
+<?php include("section/foot.php");?>
---
+++ section/dashboard.php
@@ -0,0 +1,212 @@
+<div class="breadcrumbs_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcrumb_content">
+                        <ul>
+                            <li><a href="/">home</a></li>
+                            <li>Dashboard</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="account_page_bg">
+        <div class="container">
+            <section class="main_content_area">
+                <div class="account_dashboard">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-3 col-lg-3">
+                            <!-- Nav tabs -->
+                            <div class="dashboard_tab_button">
+                                <ul role="tablist" class="nav flex-column dashboard-list">
+                                    <li><a href="#dashboard" data-toggle="tab" class="nav-link active show">Dashboard</a></li>
+                                    <li> <a href="#orders" data-toggle="tab" class="nav-link">Orders</a></li>
+                                    <li><a href="#wishlist" data-toggle="tab" class="nav-link">Wish list</a></li>
+                                    <li><a href="#address" data-toggle="tab" class="nav-link">Addresses</a></li>
+                                    <li><a href="#account-details" data-toggle="tab" class="nav-link">Account details</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-9 col-lg-9">
+                            <!-- Tab panes -->
+                            <div class="tab-content dashboard_content">
+                                <div class="tab-pane fade active show" id="dashboard">
+                                    <h3>Dashboard </h3>
+                                    <p>From your account dashboard. you can easily check &amp; view your <a href="#">recent orders</a>, manage your <a href="#">shipping and billing addresses</a> and <a href="#">Edit your password and account details.</a></p>
+                                </div>
+                                <div class="tab-pane fade" id="orders">
+                                    <h3>Orders</h3>
+                                    <div class="table-responsive">
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th>Order</th>
+                                                    <th>Date</th>
+                                                    <th>Status</th>
+                                                    <th>Total</th>
+                                                    <th>Actions</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>May 10, 2018</td>
+                                                    <td><span class="success">Completed</span></td>
+                                                    <td>$25.00 for 1 item </td>
+                                                    <td><a href="cart.html" class="view">view</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>May 10, 2018</td>
+                                                    <td><span class="success">Completed</span></td>
+                                                    <td>$25.00 for 1 item </td>
+                                                    <td><a href="cart.html" class="view">view</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>May 10, 2018</td>
+                                                    <td><span class="success">Completed</span></td>
+                                                    <td>$25.00 for 1 item </td>
+                                                    <td><a href="cart.html" class="view">view</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>May 10, 2018</td>
+                                                    <td><span class="success">Completed</span></td>
+                                                    <td>$25.00 for 1 item </td>
+                                                    <td><a href="cart.html" class="view">view</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>2</td>
+                                                    <td>May 10, 2018</td>
+                                                    <td>Processing</td>
+                                                    <td>$17.00 for 1 item </td>
+                                                    <td><a href="cart.html" class="view">view</a></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                
+                                <div class="tab-pane" id="address">
+                                    <p>The following addresses will be used on the checkout page by default.</p>
+                                    <h4 class="billing-address">Billing address</h4>
+                                    <a href="#" class="view">Edit</a>
+                                    <p><strong>Bobby Jackson</strong></p>
+                                    <address>
+                                        House #15, 
+                                        Road #1, 
+                                        Block #C ,
+                                        Banasree <br>
+                                        Dhaka ,
+                                        1212
+                                    </address>
+                                        <p>Bangladesh</p>
+                                </div>
+
+                                <div class="tab-pane" id="wishlist">
+                                <div class="table_desc wishlist">
+                                    <div class="cart_page table-responsive">
+                                        <table>
+                                            <thead>
+                                                <tr>
+                                                    <th class="product_remove">Delete</th>
+                                                    <th class="product_thumb">Image</th>
+                                                    <th class="product_name">Product</th>
+                                                    <th class="product-price">Price</th>
+                                                    <th class="product_quantity">Stock Status</th>
+                                                    <th class="product_total">Add To Cart</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="product_remove"><a href="#">X</a></td>
+                                                    <td class="product_thumb"><a href="#"><img src="assets/img/s-product/product.jpg" alt=""></a></td>
+                                                    <td class="product_name"><a href="#">Handbag fringilla</a></td>
+                                                    <td class="product-price">£65.00</td>
+                                                    <td class="product_quantity">In Stock</td>
+                                                    <td class="product_total"><a href="#">Add To Cart</a></td>
+
+
+                                                </tr>
+
+                                                <tr>
+                                                    <td class="product_remove"><a href="#">X</a></td>
+                                                    <td class="product_thumb"><a href="#"><img src="assets/img/s-product/product2.jpg" alt=""></a></td>
+                                                    <td class="product_name"><a href="#">Handbags justo</a></td>
+                                                    <td class="product-price">£90.00</td>
+                                                    <td class="product_quantity">In Stock</td>
+                                                    <td class="product_total"><a href="#">Add To Cart</a></td>
+
+
+                                                </tr>
+                                                <tr>
+                                                    <td class="product_remove"><a href="#">X</a></td>
+                                                    <td class="product_thumb"><a href="#"><img src="assets/img/s-product/product3.jpg" alt=""></a></td>
+                                                    <td class="product_name"><a href="#">Handbag elit</a></td>
+                                                    <td class="product-price">£80.00</td>
+                                                    <td class="product_quantity">In Stock</td>
+                                                    <td class="product_total"><a href="#">Add To Cart</a></td>
+
+
+                                                </tr>
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
+                                </div>
+                                
+                                <div class="tab-pane fade" id="account-details">
+                                    <h3>Account details </h3>
+                                    <div class="login">
+                                        <div class="login_form_container">
+                                            <div class="account_login_form">
+                                                <form action="#">
+                                                    <div class="input-radio">
+                                                        <span class="custom-radio"><input type="radio" value="1" name="id_gender"> Mr.</span>
+                                                        <span class="custom-radio"><input type="radio" value="1" name="id_gender"> Mrs.</span>
+                                                    </div> <br>
+                                                    <label>First Name</label>
+                                                    <input type="text" name="first-name">
+                                                    <label>Last Name</label>
+                                                    <input type="text" name="last-name">
+                                                    <label>Email</label>
+                                                    <input type="text" name="email-name">
+                                                    <label>Password</label>
+                                                    <input type="password" name="user-password">
+                                                    <label>Birthdate</label>
+                                                    <input type="text" placeholder="MM/DD/YYYY" value="" name="birthday">
+                                                    <span class="example">
+                                                      (E.g.: 05/31/1970)
+                                                    </span>
+                                                    <br>
+                                                    <span class="custom_checkbox">
+                                                        <input type="checkbox" value="1" name="optin">
+                                                        <label>Receive offers from our partners</label>
+                                                    </span>
+                                                    <br>
+                                                    <span class="custom_checkbox">
+                                                        <input type="checkbox" value="1" name="newsletter">
+                                                        <label>Sign up for our newsletter<br><em>You may unsubscribe at any moment. For that purpose, please find our contact info in the legal notice.</em></label>
+                                                    </span>
+                                                    <div class="save_button primary_btn default_button">
+                                                        <button type="submit">Save</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
